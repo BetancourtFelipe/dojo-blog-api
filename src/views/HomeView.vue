d5d2c432d6f6dd6a92742d598a9ca318a37b92c2
Felipe Palacio <felipepalaciobetancourt@gmail.com>
2024-01-30 10:08:21 +0100

    Add computed properties

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,10 @@
 <template>
 <div class="home">Home
   <h1>Home</h1>
+  <input type="text" v-model="search">
+  <p>search term - {{ search }}</p>
+  <div v-for="name in matchingNames" :key="name">{{ name }}</div>
+
      <!-- Display info for ninjaOne using refs -->
   <!-- <h2>Refs</h2>
   <p>{{ ninjaOne.name }} - {{ ninjaOne.age }}</p>
@@ -21,49 +25,60 @@
 </template>
 
 <script>
-import { reactive, ref } from 'vue';
+import { computed, ref } from 'vue';
 
 export default {
   name: 'HomeView',
   setup(){
+    const search = ref('')
+    const names = ref(['ivo', 'lukas', 'jalal', 'felipe', 'mandla', 'fabio', 'hoshi'])
+
+    const matchingNames = computed(() =>{
+      return names.value.filter((name) => name.includes(search.value))
+    })
+
+
+
+    return{ names, search, matchingNames }
+
      // Define reactive and ref properties
-    const ninjaOne = ref({ name: 'felipe', age: 29 })
-    const ninjaTwo = reactive({ name: 'lukas', age: 19})
+    // const ninjaOne = ref({ name: 'felipe', age: 29 })
+    // const ninjaTwo = reactive({ name: 'lukas', age: 19})
 
        // Function to update ninjaOne's age using refs
-    const updateNinjaOne = () => {
-      ninjaOne.value.age = 40
-    }
+    // const updateNinjaOne = () => {
+    //   ninjaOne.value.age = 40
+    // }
 
-    const updateNinjaTwo = () => {
-      ninjaTwo.age = 55
-    }
+    // const updateNinjaTwo = () => {
+    //   ninjaTwo.age = 55
+    // }
 
 
     // Creating a ref named 'p' with an initial value of 'hello'
-    const p = ref('hello')
+    // const p = ref('hello')
 
     // Logging the ref object 'p' and its current value to the console
-    console.log(p, p.value)
+    // console.log(p, p.value)
 
     // Creating refs for 'name' and 'age' with initial values
-    const name = ref('felipe')
-    const age = ref(37)
+    // const name = ref('felipe')
+    // const age = ref(37)
 
     // Defining a function named 'handleClick'
-    const handleClick = () => {
+    // const handleClick = () => {
       // Logging the ref object 'p' and its current value to the console
-      console.log(p, p.value)
+      // console.log(p, p.value)
 
      // Adding a CSS class 'test' to the DOM element referred to by 'p'
-      p.value.classList.add('test')
+      // p.value.classList.add('test')
      // Changing the text content of the DOM element referred to by 'p'
-      p.value.textContent= 'hello, ninjas'
+      // p.value.textContent= 'hello, ninjas'
 
     //    Changing the values of 'name' and 'age' refs
 
-      name.value = 'lukas'
-      age.value = 36
+      // name.value = 'lukas'
+      // age.value = 36
 
     }
 
@@ -71,9 +86,6 @@ export default {
 // It initializes refs for strings ('p' and 'name') and a ref for a number ('age').
 // The handleClick function showcases how to interact with the values referred to by refs.
 // It logs values to the console, modifies the DOM element class and text content, and updates 'name' and 'age'.
-
-
-    return { ninjaOne, ninjaTwo, updateNinjaOne, updateNinjaTwo}
-  }
 }
+
 </script>
