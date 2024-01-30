4d60205acd6911e3929a46706b1dd2bd18f75a49
Felipe Palacio <felipepalaciobetancourt@gmail.com>
2024-01-30 19:22:12 +0100

    Add getPost fetch composables

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import Details from '../views/Details.vue';
 import HomeView from '../views/HomeView.vue';
 
 const routes = [
@@ -7,6 +8,12 @@ const routes = [
     name: 'home',
     component: HomeView
   },
+  {
+    path: '/posts/:id',
+    name: 'Details',
+    component: Details,
+    props: true
+  }
 
 ]
 
